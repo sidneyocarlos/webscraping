0d9ce9dfb677bff0a7d1b6ca375bd7505dc0c55d
Carlos Sidney <kaka.cson@gmail.com>
2022-01-25 22:36:38 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://pt.wikipedia.org/wiki/Oscar_de_melhor_filme';
+
+async function getMovies() {
+	const { data } = await axios.get(url);
+	const $ = cheerio.load(data);
+
+	const list = [];
+	$('.wikitable tbody tr').each((i, elem)=>{
+		const name = $(elem).find(`td[style*="background:#FAEB86;"]`).last().text().replace("\n", "");
+		if (name !== "") {
+			const year = $(elem).find(`td[style*="background:#FAEB86;"]`).first().prev("td").text().slice(-4).replace("\n", "");
+			movie = { name, year };
+			list.push(movie);
+		}
+	});
+
+	console.log(list)
+}
+
+getMovies();
